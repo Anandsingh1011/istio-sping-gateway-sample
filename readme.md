7ba5532481369d1239457d3fb8c3a3351e277cce
Anand <Anandsingh1011@users.noreply.github.com>
2020-05-24 13:29:55 +0530

    Initial commit

---
+++ readme.md
@@ -0,0 +1,83 @@
+# istio-sping-gateway-sample
+Simple Example of Istio Ingress using kubernetes via minikibe and spring boot service.
+
+
+### Install Istio
+
+
+curl -L https://istio.io/downloadIstio | sh -
+
+cd istio-1.6.0
+
+export PATH=$PWD/bin:$PATH
+
+istioctl install --set addonComponents.grafana.enabled=true
+
+istioctl install --set values.tracing.enabled=true
+
+istioctl install --set values.tracing.ingress.enabled=true 
+ 
+istioctl manifest apply --set values.kiali.enabled=true
+
+kubectl label namespace default istio-injection=enabled
+
+
+### Open Dashboard
+
+
+minikube dashboard
+istioctl dashboard kiali
+istioctl dashboard jaeger
+
+
+### Deploy Application 
+
+git clone https://github.com/Anandsingh1011/istio-sping-gateway-sample.git
+
+cd istio-sping-gateway-sample/caller-service
+
+skaffold run # This will build and deploy application in minikube
+
+
+cd istio-sping-gateway-sample/callme-service
+
+skaffold run # This will build and deploy application in minikube
+
+
+
+Note : To see log while deploying run below
+skaffold run --tail --no-prune=false --cache-artifacts=false   
+
+
+Now you can access application
+
+
+### To Expose Istio-Gateway 
+
+1. Run this command in a new terminal window to start a Minikube tunnel that sends traffic to your Istio Ingress Gateway:
+
+minikube tunnel
+
+2. Get IP
+
+minikube ip
+
+INGRESS_IP
+
+3. Get PORT
+
+INGRESS_PORT
+
+kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].nodePort}'
+
+
+4. Run below curl
+
+curl -v  -H 'Host: caller.example.com' http://INGRESS_IP:INGRESS_PORT/caller/ping 
+
+5. To create load run below 
+
+
+for ((i=1;i<=1000;i++)); do   curl -v  -H 'Host: caller.example.com' http://192.168.64.11:32621/caller/ping ; done
+
+
---
+++ spring-boot-istio/src/main/java/com/github/piomin/springboot/istio/EnableIstioAnnotationProcessor.java
@@ -0,0 +1,113 @@
+package com.github.piomin.springboot.istio;
+
+import me.snowdrop.istio.api.Duration;
+import me.snowdrop.istio.api.networking.v1alpha3.*;
+import me.snowdrop.istio.client.IstioClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+
+import java.util.Optional;
+
+public class EnableIstioAnnotationProcessor implements BeanPostProcessor {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(EnableIstioAnnotationProcessor.class);
+    private ConfigurableListableBeanFactory configurableBeanFactory;
+    private IstioClient istioClient;
+    private IstioService istioService;
+
+    public EnableIstioAnnotationProcessor(ConfigurableListableBeanFactory configurableBeanFactory, IstioClient istioClient, IstioService istioService) {
+        this.configurableBeanFactory = configurableBeanFactory;
+        this.istioClient = istioClient;
+        this.istioService = istioService;
+    }
+
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        Class<?> managedBeanClass = bean.getClass();
+        EnableIstio enableIstioAnnotation =  managedBeanClass.getAnnotation(EnableIstio.class);
+        if (enableIstioAnnotation != null) {
+            LOGGER.info("Istio feature enabled: {}", enableIstioAnnotation);
+            DestinationRule dr = istioClient.destinationRule().withName(istioService.getApplicationName() + "-destination").get();
+            if (dr == null) {
+                dr = new DestinationRuleBuilder().withApiVersion("networking.istio.io/v1alpha3")
+                        .withNewMetadata().withName(istioService.getApplicationName() + "-destination").endMetadata()
+                        .withNewSpec()
+                            .withNewHost(istioService.getApplicationName())
+                            .withSubsets(new SubsetBuilder().withNewName("v1").addToLabels("version", "v1").build())
+                            .withTrafficPolicy(new TrafficPolicyBuilder()
+                                    .withOutlierDetection(new OutlierDetectionBuilder()
+                                            .withConsecutiveErrors(10)
+                                            .withBaseEjectionTime(new Duration(0, 30000L))
+                                            .build())
+                                    .build())
+                        .endSpec()
+                        .build();
+                istioClient.destinationRule().create(dr);
+                LOGGER.info("New DestinationRule created: {}", dr);
+            } else {
+                LOGGER.info("Found DestinationRule: {}", dr);
+                if (!enableIstioAnnotation.version().isEmpty()) {
+                    Optional<Subset> subset = dr.getSpec().getSubsets().stream()
+                            .filter(s -> s.getName().equals(enableIstioAnnotation.version()))
+                            .findAny();
+                    if (subset.isEmpty()) {
+                        istioClient.destinationRule().withName(istioService.getApplicationName() + "-destination")
+                                .edit()
+                                .editSpec()
+                                .addNewSubset()
+                                    .withNewName(enableIstioAnnotation.version())
+                                    .addToLabels("version", enableIstioAnnotation.version())
+                                .endSubset()
+                                .endSpec()
+                                .done();
+                    }
+                }
+            }
+            VirtualService vs = istioClient.virtualService().withName(istioService.getApplicationName() + "-route").get();
+            if (vs == null) {
+                 vs = new VirtualServiceBuilder().withApiVersion("networking.istio.io/v1alpha3")
+                        .withNewMetadata().withName(istioService.getApplicationName() + "-route").endMetadata()
+                        .withNewSpec()
+                            .addToHosts(istioService.getApplicationName())
+                            .addNewHttp()
+                                .withTimeout(enableIstioAnnotation.timeout() == 0 ? null : new Duration(0, (long) enableIstioAnnotation.timeout()))
+                                .withRetries(enableIstioAnnotation.numberOfRetries() == 0 ? null :
+                                        new HTTPRetryBuilder()
+                                                .withAttempts(enableIstioAnnotation.numberOfRetries())
+                                                .withRetryOn("5xx")
+                                                .withPerTryTimeout(new Duration(0, 1000L))
+                                                .build())
+                                .addNewRoute()
+                                    .withNewDestination().withHost(istioService.getApplicationName()).withSubset("v1").endDestination()
+                                .endRoute()
+                            .endHttp()
+                        .endSpec()
+                        .build();
+                istioClient.virtualService().create(vs);
+                LOGGER.info("New VirtualService created: {}", vs);
+            } else {
+                LOGGER.info("Found VirtualService: {}", vs);
+                if (!enableIstioAnnotation.version().isEmpty()) {
+                    istioClient.virtualService().withName(istioService.getApplicationName() + "-route")
+                            .edit()
+                            .editSpec()
+                            .editFirstHttp()
+                                .withTimeout(enableIstioAnnotation.timeout() == 0 ? null : new Duration(0, (long) enableIstioAnnotation.timeout()))
+                                .withRetries(enableIstioAnnotation.numberOfRetries() == 0 ? null :
+                                        new HTTPRetryBuilder().withAttempts(enableIstioAnnotation.numberOfRetries()).build())
+                            .editFirstRoute()
+                                .withWeight(enableIstioAnnotation.weight() == 0 ? null: enableIstioAnnotation.weight())
+                                .withNewDestination().withHost(istioService.getApplicationName()).withSubset(enableIstioAnnotation.version()).endDestination()
+                            .endRoute()
+                            .endHttp()
+                            .endSpec()
+                            .done();
+                }
+            }
+        }
+        return bean;
+    }
+
+}
